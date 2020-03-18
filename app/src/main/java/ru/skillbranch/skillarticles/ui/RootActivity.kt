d55b303ed14d8b83cd17b9ce86c7193c4d7fbbdb
Raman Skaskevich <ras@novelpay.pl>
2020-03-18 16:24:44 +0100

    Added toolbar and bottombar

--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -1,13 +1,34 @@
 package ru.skillbranch.skillarticles.ui
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_root.*
 import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.extensions.dpToIntPx
 
 class RootActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_root)
+    setupToolbar()
+  }
+
+  private fun setupToolbar() {
+    setSupportActionBar(toolbar)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    val logo = if (toolbar.childCount > 2) toolbar.getChildAt(2) as ImageView else null
+    logo?.scaleType = ImageView.ScaleType.CENTER_CROP
+
+    val lp = logo?.layoutParams as Toolbar.LayoutParams
+    lp?.let {
+      it.width = this.dpToIntPx(40)
+      it.height = this.dpToIntPx(40)
+      it.marginEnd = this.dpToIntPx(16)
+      logo.layoutParams = it
+    }
   }
 }
---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
@@ -0,0 +1,17 @@
+package ru.skillbranch.skillarticles.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import ru.skillbranch.skillarticles.R
+
+class Bottombar @JvmOverloads constructor(
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+  init {
+    View.inflate(context, R.layout.layout_bottom_bar, this)
+  }
+}

2232a82c0e596821ab0525ce1f651638d79f27b9
Raman Skaskevich <ras@novelpay.pl>
2020-03-25 09:36:45 +0100

    Add bottombar scroll behavior

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -0,0 +1,29 @@
+package ru.skillbranch.skillarticles.ui.custom.behaviors
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
+import java.lang.Float.max
+import java.lang.Float.min
+
+class BottombarBehavior(context: Context, attrs: AttributeSet) : CoordinatorLayout.Behavior<Bottombar>(context, attrs) {
+
+  override fun onStartNestedScroll(
+    coordinatorLayout: CoordinatorLayout, child: Bottombar, directTargetChild: View, target: View, axes: Int, type: Int
+  ): Boolean {
+    return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+  }
+
+  @RequiresApi(Build.VERSION_CODES.N)
+  override fun onNestedPreScroll(
+    coordinatorLayout: CoordinatorLayout, child: Bottombar, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int
+  ) {
+    child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+    super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+  }
+}

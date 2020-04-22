f3e05abcbd294ba998f2294401cfb9478e0dfd27
Raman Skaskevich <ras@novelpay.pl>
2020-04-22 09:34:23 +0200

    Fix indexesOf extension

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -1,6 +1,7 @@
 package ru.skillbranch.skillarticles.extensions
 
-fun String.indexesOf(substr: String, ignoreCase: Boolean = true) : List<Int> {
+fun String?.indexesOf(substr: String, ignoreCase: Boolean = true) : List<Int>? {
+  if (this == null) return null
   if (substr.isEmpty()) return emptyList()
   val content = if (ignoreCase) toLowerCase() else this
   return Regex(substr).findAll(content).map { it.range.first }.toList().filter { it != -1 }

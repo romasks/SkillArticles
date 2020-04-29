a7dcc8dda35a62b098ef52c2dfcbfa0515ee910e
Raman Skaskevich <ras@novelpay.pl>
2020-04-29 03:12:38 +0200

    Add markdown parser; Add tests stubs; Add test for unordered list

---
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -0,0 +1,104 @@
+package ru.skillbranch.skillarticles.markdown
+
+import java.util.regex.Pattern
+
+object MarkdownParser {
+
+  private val LINE_SEPARATOR = System.getProperty("line.separator") ?: "\n"
+
+  // group regex
+  private const val UNORDERED_LIST_ITEM_GROUP = "(^[*+-] .+$)"
+
+  // result regex
+  const val MARKDOWN_GROUP = "$UNORDERED_LIST_ITEM_GROUP"
+
+  private val elementsPattern by lazy { Pattern.compile(MARKDOWN_GROUP, Pattern.MULTILINE) }
+
+  /*
+  * parse markdown text to elements
+  * */
+  fun parse(string: String): MarkdownText {
+    val elements = mutableListOf<Element>()
+    elements.addAll(findElements(string))
+    return MarkdownText(elements)
+  }
+
+  /*
+  * clear markdown text to string without markdown characters
+  * */
+  fun clear(string: String): String? {
+    return null
+  }
+
+  private fun findElements(string: CharSequence): List<Element> {
+    val parents = mutableListOf<Element>()
+    val matcher = elementsPattern.matcher(string)
+    var lastStartIndex = 0
+
+    loop@while (matcher.find(lastStartIndex)) {
+      val startIndex = matcher.start()
+      val endIndex = matcher.end()
+
+      // if something is found then everything before - TEXT
+      if (lastStartIndex < startIndex) {
+        parents.add(Element.Text(string.subSequence(lastStartIndex, startIndex)))
+      }
+
+      // found text
+      var text: CharSequence
+
+      // groups range for iterate by groups
+      val groups = 1..1
+      var group = -1
+      for (gr in groups) {
+        if (matcher.group(gr) != null) {
+          group = gr
+          break
+        }
+      }
+
+      when (group) {
+        // NOT FOUND -> BREAK
+        -1 -> break@loop
+
+        // UNORDERED
+        1 -> {
+          // text without "*. "
+          text = string.subSequence(startIndex.plus(2), endIndex)
+
+          // find inner elements
+          val subs = findElements(text)
+          val element = Element.UnorderedListItem(text, subs)
+          parents.add(element)
+
+          // next find start from position "endIndex" (last regex character)
+          lastStartIndex = endIndex
+        }
+      }
+    }
+
+    if (lastStartIndex < string.length) {
+      val text = string.subSequence(lastStartIndex, string.length)
+      parents.add(Element.Text(text))
+    }
+
+    return parents
+  }
+}
+
+data class MarkdownText(val elements: List<Element>)
+
+sealed class Element() {
+  abstract val text: CharSequence
+  abstract val elements: List<Element>
+
+  data class Text(
+    override val text: CharSequence,
+    override val elements: List<Element> = emptyList()
+  ) : Element()
+
+  data class UnorderedListItem(
+    override val text: CharSequence,
+    override val elements: List<Element> = emptyList()
+  ) : Element()
+}
--- app/src/test/java/ru/skillbranch/skillarticles/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/skillarticles/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package ru.skillbranch.skillarticles
 
+import org.junit.Assert
 import org.junit.Test
-
-import org.junit.Assert.*
+import ru.skillbranch.skillarticles.markdown.Element
+import ru.skillbranch.skillarticles.markdown.MarkdownParser
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,51 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
   @Test
-  fun addition_isCorrect() {
-    assertEquals(4, 2 + 2)
+  fun parse_list_items() {
+    val result = MarkdownParser.parse(unorderedListString)
+    val actual = prepare<Element.UnorderedListItem>(result.elements)
+    Assert.assertEquals(expectedUnorderedList, actual)
+
+    printResults(actual)
+    println("")
+    printElements(result.elements)
+  }
+
+  private fun printResults(list: List<String>) {
+    val iterator = list.iterator()
+    while (iterator.hasNext()) {
+      println("find >> ${iterator.next()}")
+    }
+  }
+
+  private fun printElements(list: List<Element>) {
+    val iterator = list.iterator()
+    while (iterator.hasNext()) {
+      println("element >> ${iterator.next()}")
+    }
+  }
+
+  private fun Element.spread(): List<Element> {
+    val elements = mutableListOf<Element>()
+    elements.add(this)
+    elements.addAll(this.elements.spread())
+    return elements
+  }
+
+  private fun List<Element>.spread(): List<Element> {
+    val elements = mutableListOf<Element>()
+    if (this.isNotEmpty()) elements.addAll(
+      this.fold(mutableListOf()) { acc, el -> acc.also { it.addAll(el.spread()) } }
+    )
+    return elements
+  }
+
+  private inline fun <reified T : Element> prepare(list: List<Element>): List<String> {
+    return list
+      .fold(mutableListOf<Element>()) { acc, el ->
+        acc.also { it.addAll(el.spread()) } // spread inner elements
+      }
+      .filterIsInstance<T>() // filter only expected instance
+      .map { it.text.toString() } // transform to element text
   }
 }

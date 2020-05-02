48ac8a838ed52965cb3e411eae598084e7df5e04
Raman Skaskevich <ras@novelpay.pl>
2020-05-02 02:03:20 +0200

    Implement HeaderSpan with UI test

--- app/src/androidTest/java/ru/skillbranch/skillarticles/Hometask5InstrumentedTest.kt
+++ app/src/androidTest/java/ru/skillbranch/skillarticles/Hometask5InstrumentedTest.kt
@@ -6,6 +6,7 @@ import android.graphics.Paint
 import android.text.Layout
 import android.text.SpannableString
 import android.text.Spanned
+import android.text.TextPaint
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -13,7 +14,9 @@ import org.junit.runner.RunWith
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.inOrder
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 import ru.skillbranch.skillarticles.markdown.spans.BlockquotesSpan
+import ru.skillbranch.skillarticles.markdown.spans.HeaderSpan
 import ru.skillbranch.skillarticles.markdown.spans.UnorderedListSpan
 
 /**
@@ -123,6 +126,67 @@ class Hometask5InstrumentedTest {
       paint
     )
     // check paint color restore
+    inOrder.verify(paint).color = defaultColor
+  }
+
+  @Test
+  fun draw_header() {
+    // settings
+    val level = 1
+    val textColor = Color.RED
+    val lineColor = Color.GREEN
+    val marginTop = 24f
+    val marginBottom = 16f
+
+    // defaults
+    val canvasWidth = 700
+    val defaultColor = Color.GRAY
+    val cml = 0 // current margin location
+    val ltop = 0 // line top
+    val lbase = 60 // line baseline
+    val lbottom = 60 // line bottom
+
+    // mock
+    val canvas = mock(Canvas::class.java)
+    `when`(canvas.width).thenReturn(canvasWidth)
+    val paint = mock(Paint::class.java)
+    `when`(paint.color).thenReturn(defaultColor)
+    val measurePaint = mock(TextPaint::class.java)
+    val drawPaint = mock(TextPaint::class.java)
+    val layout = mock(Layout::class.java)
+
+    val text = SpannableString("text")
+
+    val span = HeaderSpan(level, textColor, lineColor, marginTop, marginBottom)
+    text.setSpan(span, 0, text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+    // check leading margin
+    assertEquals(0, span.getLeadingMargin(true))
+
+    // check measure state
+    span.updateMeasureState(measurePaint)
+    verify(measurePaint).textSize *= span.sizes[level]!!
+    verify(measurePaint).isFakeBoldText = true
+
+    // check draw state
+    span.updateDrawState(drawPaint)
+    verify(drawPaint).textSize *= span.sizes[level]!!
+    verify(drawPaint).isFakeBoldText = true
+    verify(drawPaint).color = textColor
+
+    // check line draw
+    span.drawLeadingMargin(
+      canvas, paint, cml, 1, ltop, lbase, lbottom,
+      text, 0, text.length, true, layout
+    )
+
+    // check order call
+    val inOrder = inOrder(paint, canvas)
+    // check first set color to paint
+    inOrder.verify(paint).color = lineColor
+    val lh = (paint.descent() - paint.ascent()) * span.sizes[level]!!
+    val lineOffset = lbase + lh * span.linePadding
+    inOrder.verify(canvas).drawLine(0f, lineOffset, canvasWidth.toFloat(), lineOffset, paint)
     inOrder.verify(paint).color = defaultColor
   }
 }
--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
@@ -11,14 +11,20 @@ import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.extensions.attrValue
 import ru.skillbranch.skillarticles.extensions.dpToPx
 import ru.skillbranch.skillarticles.markdown.spans.BlockquotesSpan
+import ru.skillbranch.skillarticles.markdown.spans.HeaderSpan
 import ru.skillbranch.skillarticles.markdown.spans.UnorderedListSpan
 
 class MarkdownBuilder(context: Context) {
 
+  private val colorSecondary = context.attrValue(R.attr.colorSecondary)
+  private val colorPrimary = context.attrValue(R.attr.colorPrimary)
+  private val colorDivider = context.attrValue(R.color.color_divider)
+
   private val gap = context.dpToPx(8)
   private val bulletRadius = context.dpToPx(4)
   private val quoteWidth = context.dpToPx(4)
-  private val colorSecondary = context.attrValue(R.attr.colorSecondary)
+  private val headerMarginTop = context.dpToPx(12)
+  private val headerMarginBottom = context.dpToPx(8)
 
   fun markdownToSpan(string: String): SpannedString {
     val markdown = MarkdownParser.parse(string)
@@ -46,6 +52,11 @@ class MarkdownBuilder(context: Context) {
             for (child in element.elements) {
               buildElement(child, builder)
             }
+          }
+        }
+        is Element.Header -> {
+          inSpans(HeaderSpan(element.level, colorPrimary, colorDivider, headerMarginTop, headerMarginBottom)) {
+            append(element.text)
           }
         }
         else -> append(element.text)
